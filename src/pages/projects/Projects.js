60923df224b18fcf71e7056cf1f721e91e08f0ea
Junhyeong Park <oakr0414@my.yorku.ca>
2023-06-21 18:16:41 -0400

    Initial commit

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -4,7 +4,6 @@ import Footer from "../../components/footer/Footer";
 import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
 import Button from "../../components/button/Button";
 import TopButton from "../../components/topButton/TopButton";
-import { Roll } from "react-reveal";
 import {
   greet,
   projectsHeader,
@@ -20,31 +19,29 @@ class Projects extends Component {
       <div className="projects-main">
         <Header theme={theme} />
         <div className="basic-projects">
-          <Roll bottom duration={2000} distance="40px">
-            <div className="projects-heading-div">
-              <div className="projects-heading-img-div">
-                {/* <img
+          <div className="projects-heading-div">
+            <div className="projects-heading-img-div">
+              {/* <img
 											src={require(`../../assests/images/${projectsHeader["avatar_image_path"]}`)}
 											alt=""
 										/> */}
-                <ProjectsImg theme={theme} />
-              </div>
-              <div className="projects-heading-text-div">
-                <h1
-                  className="projects-heading-text"
-                  style={{ color: theme.text }}
-                >
-                  {projectsHeader.title}
-                </h1>
-                <p
-                  className="projects-header-detail-text subTitle"
-                  style={{ color: theme.secondaryText }}
-                >
-                  {projectsHeader["description"]}
-                </p>
-              </div>
+              <ProjectsImg theme={theme} />
             </div>
-          </Roll>
+            <div className="projects-heading-text-div">
+              <h1
+                className="projects-heading-text"
+                style={{ color: theme.text }}
+              >
+                {projectsHeader.title}
+              </h1>
+              <p
+                className="projects-header-detail-text subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                {projectsHeader["description"]}
+              </p>
+            </div>
+          </div>
         </div>
         <div className="repo-cards-div-main">
           {ProjectsData.data.map((repo) => {
